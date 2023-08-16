1fc6c02c35032a37ab19c31ee5b9276b0e2cb760
Hafidz <hafidz.harifin@gmail.com>
2023-08-16 13:44:52 +0200

    Update order and path

--- analysis/plot/plot_matching.py
+++ analysis/plot/plot_matching.py
@@ -6,18 +6,31 @@ import seaborn as sns
 
 if __name__ == "__main__":
     # Read the CSV file into a pandas DataFrame
-    csv_file = '../matching_merged.csv'
+    csv_file = '../../data/results_corrected_format/matching_merged.csv'
     data = pd.read_csv(csv_file)
 
     # Set up Seaborn for better aesthetics
     sns.set(style="whitegrid")
+    stim_order= [
+    "sbc",
+    "bullseye_high_freq",
+    "sbc_separate",
+    "bullseye_low_separate",
+    "whites",
+    "whites_separate",
+    "strip",
+    "checkerboard",
+    "checkerboard_separate",
+    "catch_trial_black_3",
+    "catch_trial_white_3",
+]
 
     # Group data by 'stim' and 'target_side', then create box plots
     plt.figure(figsize=(10, 6))  # Adjust the figure size as needed
-    sns.boxplot(x='intensity_match', y='stim', hue='target_side', data=data, orient='h')
+    sns.boxplot(x='intensity_match', y='stim', hue='target_side', order=stim_order, data=data, orient='h')
     sns.despine(left=True)
     # Add individual data points
-    sns.stripplot(x='intensity_match', y='stim', hue='target_side', data=data, jitter=False, dodge=True, size=3.5, orient='h')
+    sns.stripplot(x='intensity_match', y='stim', hue='target_side', order=stim_order, data=data, jitter=False, dodge=True, size=3.5, orient='h')
 
     plt.xlabel('Adjusted brightness by subjects')
     plt.ylabel('Stimulus')
