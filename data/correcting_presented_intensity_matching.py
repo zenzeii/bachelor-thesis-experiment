ca12951f82cee10b6147b28e34f553478f26c7c5
Hafidz <hafidz.harifin@gmail.com>
2023-08-16 13:43:51 +0200

    Add correction for likert

--- data/correcting_presented_intensity_matching.py
+++ data/correcting_presented_intensity_matching.py
@@ -2,14 +2,14 @@ import os
 import pandas as pd
 import shutil
 
-def replace_nan_in_csv(file_path):
+def replace_nan_in_matching_csv(file_path):
     df = pd.read_csv(file_path)
     if 'matching' in file_path:
         df['matching_flipped'] = df['matching_flipped'].fillna(False)
         df['presented_intensity'] = df['presented_intensity'].fillna(0.5)
     df.to_csv(file_path, index=False)
 
-def replace_target_side_in_csv(file_path):
+def replace_target_side_in_matching_csv(file_path):
     df = pd.read_csv(file_path)
     if 'matching' in file_path:
         for index, row in df.iterrows():
@@ -18,8 +18,29 @@ def replace_target_side_in_csv(file_path):
                 df.at[index, 'stim'] = row['stim'].split('_')[0] + '_' + row['stim'].split('_')[1] + '_' + row['stim'].split('_')[3] + '_' + row['stim'].split('_')[2]
         df.to_csv(file_path, index=False)
 
+def flip_response_values_in_likert_csv(file_path):
+    df = pd.read_csv(file_path)
+    if 'direction' in file_path:
+        for index, row in df.iterrows():
+            if row['likert_flipped'] == 'True':
+                df.at[index, 'response'] = flip_values(row['response'])
+        df.to_csv(file_path, index=False)
+
+def flip_values(value):
+    if value == '1':
+        return '5'
+    elif value == '2':
+        return '4'
+    elif value == '3':
+        return '3'
+    elif value == '4':
+        return '2'
+    elif value == '5':
+        return '1'
+
+
 def process_folders(source_folder, target_folder):
-    for root, dirs, files in os.walk(source_folder):
+    for root, dirs, fireplace_nan_in_matching_csvles in os.walk(source_folder):
         for dir_name in dirs:
             source_dir = os.path.join(root, dir_name)
             target_dir = os.path.join(target_folder, source_dir[len(source_folder) + 1:])
@@ -30,8 +51,9 @@ def process_folders(source_folder, target_folder):
                         source_file = os.path.join(root2, file_name)
                         target_file = os.path.join(target_dir, file_name)
                         shutil.copy(source_file, target_file)
-                        replace_nan_in_csv(target_file)
-                        replace_target_side_in_csv(target_file)
+                        replace_nan_in_matching_csv(target_file)
+                        replace_target_side_in_matching_csv(target_file)
+                        flip_response_values_in_likert_csv(target_file)
 
 
 if __name__ == "__main__":
