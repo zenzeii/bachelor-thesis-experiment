107dca9bf8ef291b4c4345dd354d99026f99f2f6
Hafidz <hafidz.harifin@gmail.com>
2023-08-20 19:05:20 +0200

    Fix avg_response_distribution

--- analysis/direction_analysis/likert/likert_analysis.py
+++ analysis/direction_analysis/likert/likert_analysis.py
@@ -65,36 +65,58 @@ def avg_response_distribution(df, intensities):
     # Filter rows with intensities
     df = df[df['presented_intensity'].isin(intensities)].copy()
 
-    # Define color mapping based on response values
-    color_mapping = {
-        5: 'red',
-        4: 'lightcoral',
-        3: 'lightgrey',
-        2: 'lightblue',
-        1: 'blue'
+    # Define a colormap for red-blue gradient
+    cmap = sns.diverging_palette(250, 10, as_cmap=True)
+
+    # Define the color map based on likert_flipped values
+    color_map = {
+        1: cmap(0.1),  # Far start of the gradient for extreme values
+        2: cmap(0.4),
+        3: cmap(0.5),  # Middle of the gradient
+        4: cmap(0.6),
+        5: cmap(0.9)   # Pick colors from the far end of the gradient for extreme values
     }
+
+    legend = ['Left target is definitely brighter', 'Left target is maybe brighter', 'Targets are equally bright', 'Right target is maybe brighter', 'Right target is definitely brighter']
 
     # Process the data to group and aggregate by stim and response
     grouped_data = df.groupby(['stim', 'response']).size().unstack(fill_value=0)
 
     # Sort the data by response values
-    sorted_data = grouped_data.sort_values(by=list(color_mapping.keys()), ascending=True)
+    sorted_data = grouped_data.sort_values(by=list(color_map.keys()), ascending=True)
 
-    # Plot the horizontal bar graph
-    plt.figure(figsize=(10, 8))
-    sns.set()
+    # Create a horizontal bar chart
+    fig, ax = plt.subplots(figsize=(10, 8))
 
-    for idx, (stim, row) in enumerate(sorted_data.iterrows()):
-        color = [color_mapping[resp] for resp in row.index]
-        plt.barh(idx, row, color=color, edgecolor='black')
+    y_labels = sorted_data.index.tolist()
 
-    plt.yticks(range(len(sorted_data)), sorted_data.index)
-    plt.ylabel("Stimulus")
-    plt.xlabel("Number of Trials")
-    plt.title("Response Distribution by Stimulus")
+    bar_width = 0.6
+    bottoms = np.zeros(len(y_labels))  # To keep track of the bottom positions for stacking
+
+    for i, likert_value in enumerate(sorted_data.columns):
+        color = color_map.get(likert_value, 'white')
+        counts = sorted_data[likert_value]
+        bars = ax.barh(np.arange(len(y_labels)), counts, color=color, label=legend[int(likert_value)-1], height=bar_width,
+                       left=bottoms)
+
+        for bar, value in zip(bars, counts):
+            if value != 0:  # Only label bars that have a count
+                ax.text(bar.get_x() + bar.get_width() / 2, bar.get_y() + bar.get_height() / 2, str(value),
+                        ha='center', va='center', color='black', fontsize=10)
+
+        bottoms += counts  # Update the bottoms for the next iteration
+
+    ax.set_yticks(np.arange(len(y_labels)))
+    ax.set_yticklabels(y_labels)
+    ax.get_xaxis().set_visible(False)
+    ax.set_title('Distribution of Responses for Each Stimulus')
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, 0))
+
+    plt.tight_layout()
 
     # Show the plot
     plt.savefig(f'avg_response_distribution_{intensities}.png')
+
 
 if __name__ == "__main__":
 
