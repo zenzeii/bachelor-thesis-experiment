9eaa6dbdb85f81c424342ff9526c048a218bae68
Hafidz <hafidz.harifin@gmail.com>
2023-08-20 19:48:38 +0200

    Syntax and comments

--- analysis/direction_analysis/likert/likert_analysis.py
+++ analysis/direction_analysis/likert/likert_analysis.py
@@ -5,143 +5,148 @@ import seaborn as sns
 
 
 def avg_response_per_stimulus(df, intensities):
-    # Filter rows with intensities
-    df = df[(df['presented_intensity']).isin(intensities)]
+    """
+    Generate a scatter plot showing the average response for each stimulus.
 
-    # Group the data by 'stim' and calculate the average response
-    avg_response = df.groupby("stim")["response"].mean()
+    Parameters:
+    - df: DataFrame containing the data
+    - intensities: List of intensities to filter by
+    """
 
-    # Sort stimuli by average response
+    # Filter rows based on given intensities
+    df_filtered = df[df['presented_intensity'].isin(intensities)]
+
+    # Group by 'stim' and compute average response
+    avg_response = df_filtered.groupby("stim")["response"].mean()
+
+    # Sort stimuli by the computed average response
     sorted_stim = avg_response.sort_values(ascending=False).index.tolist()
 
-    # Define a colormap for red-blue gradient
+    # Define a red-blue gradient colormap
     cmap = sns.diverging_palette(250, 10, as_cmap=True)
 
-    # Find the min and max response values for color normalization
+    # Determine color normalization bounds
     vmin = avg_response.min()
     vmax = avg_response.max()
 
-    # Create a scatter plot with circles
+    # Plotting
     plt.figure(figsize=(10, 6))
     for i, stim in enumerate(sorted_stim, start=1):
         x = avg_response[stim]
         y = i
-
-        # Get the color from the colormap based on the normalized response value
         norm_value = (x - vmin) / (vmax - vmin)
         color = cmap(norm_value)
 
         plt.scatter(x, y, s=2000, c=[color], alpha=0.5, label=stim)
-        plt.text(x-(0.04), y-(0.06), round(x, 2))
+        plt.text(x - (0.04), y - (0.06), round(x, 2))
 
     plt.ylabel("Stimulus")
     plt.xlabel("Average Response")
-    plt.title("Average Response per Stimulus for intensities: " + str(intensities))
+    plt.title(f"Average Response per Stimulus for intensities: {intensities}")
     plt.yticks(range(1, len(sorted_stim) + 1), sorted_stim)
     plt.xticks(range(1, 6), range(1, 6))
     plt.axvline(x=3, color="black", linestyle="--", label="Threshold")
     plt.tight_layout()
     plt.grid(True)
-
-    # Show the plot
     plt.savefig(f'avg_response_per_stimulus_{intensities}.png')
 
+
 def avg_response_per_participant(df, intensities):
-    # Filter rows with intensities
-    df = df[df['presented_intensity'].isin(intensities)].copy()
+    """
+    Generate a heatmap illustrating the average response for each participant and stimulus.
 
-    # Extract participant ID from trial
-    df['participant'] = df['trial'].str[:2]
+    Parameters:
+    - df: DataFrame containing the data
+    - intensities: List of intensities to filter by
+    """
 
-    # Pivot the data to get average response values for each participant and stimulus
-    pivot_data = df.pivot_table(index='stim', columns='participant', values='response', aggfunc='mean')
+    # Filter and preprocess the data
+    df_filtered = df[df['presented_intensity'].isin(intensities)].copy()
+    df_filtered['participant'] = df_filtered['trial'].str[:2]
+    pivot_data = df_filtered.pivot_table(index='stim', columns='participant', values='response', aggfunc='mean')
 
-    # Define a colormap for red-blue gradient
+    # Define colormap
     cmap = sns.diverging_palette(250, 10, as_cmap=True)
 
     # Create the heatmap
     plt.figure(figsize=(12, 6))
-    heatmap = sns.heatmap(pivot_data, cmap=cmap, center=3, annot=True, fmt=".2f", linewidths=0.5)
-
-    # Set heatmap title and labels
-    plt.title("Average Response Heatmap for intensities: " + str(intensities))
+    sns.heatmap(pivot_data, cmap=cmap, center=3, annot=True, fmt=".2f", linewidths=0.5)
+    plt.title(f"Average Response Heatmap for intensities: {intensities}")
     plt.xlabel("Participant")
     plt.ylabel("Stimulus")
-
-    # Adjust layout to fit within figure size
     plt.tight_layout()
-
-    # Show the plot
     plt.savefig(f'avg_response_per_participant_{intensities}.png')
 
 
 def response_distribution(df, intensities):
-    # Filter rows with intensities
-    df = df[df['presented_intensity'].isin(intensities)].copy()
+    """
+    Display the distribution of responses for each stimulus.
 
-    # Define a colormap for red-blue gradient
+    Parameters:
+    - df: DataFrame containing the data
+    - intensities: List of intensities to filter by
+    """
+
+    # Filter data based on intensities
+    df_filtered = df[df['presented_intensity'].isin(intensities)].copy()
+
+    # Colors for the likert scale responses
     cmap = sns.diverging_palette(250, 10, as_cmap=True)
-
-    # Define the color map based on likert_flipped values
     color_map = {
-        1: cmap(0.1),  # Far start of the gradient for extreme values
+        1: cmap(0.1),
         2: cmap(0.4),
-        3: cmap(0.5),  # Middle of the gradient
+        3: cmap(0.5),
         4: cmap(0.6),
-        5: cmap(0.9)   # Pick colors from the far end of the gradient for extreme values
+        5: cmap(0.9)
     }
 
-    legend = ['Left target is definitely brighter', 'Left target is maybe brighter', 'Targets are equally bright', 'Right target is maybe brighter', 'Right target is definitely brighter']
+    # Legend for the responses
+    legend = ['Left target is definitely brighter', 'Left target is maybe brighter', 'Targets are equally bright',
+              'Right target is maybe brighter', 'Right target is definitely brighter']
 
-    # Process the data to group and aggregate by stim and response
-    grouped_data = df.groupby(['stim', 'response']).size().unstack(fill_value=0)
-
-    # Sort the data by response values
+    # Aggregate data
+    grouped_data = df_filtered.groupby(['stim', 'response']).size().unstack(fill_value=0)
     sorted_data = grouped_data.sort_values(by=list(color_map.keys()), ascending=True)
 
-    # Create a horizontal bar chart
+    # Bar chart for visualization
     fig, ax = plt.subplots(figsize=(10, 8))
-
     y_labels = sorted_data.index.tolist()
-
     bar_width = 0.6
-    bottoms = np.zeros(len(y_labels))  # To keep track of the bottom positions for stacking
+    bottoms = np.zeros(len(y_labels))
 
     for i, likert_value in enumerate(sorted_data.columns):
         color = color_map.get(likert_value, 'white')
         counts = sorted_data[likert_value]
-        bars = ax.barh(np.arange(len(y_labels)), counts, color=color, label=legend[int(likert_value)-1], height=bar_width,
-                       left=bottoms)
+        bars = ax.barh(np.arange(len(y_labels)), counts, color=color, label=legend[int(likert_value) - 1],
+                       height=bar_width, left=bottoms)
 
+        # Labeling bars
         for bar, value in zip(bars, counts):
-            if value != 0:  # Only label bars that have a count
-                ax.text(bar.get_x() + bar.get_width() / 2, bar.get_y() + bar.get_height() / 2, str(value),
-                        ha='center', va='center', color='black', fontsize=10)
+            if value != 0:
+                ax.text(bar.get_x() + bar.get_width() / 2, bar.get_y() + bar.get_height() / 2, str(value), ha='center',
+                        va='center', color='black', fontsize=10)
 
-        bottoms += counts  # Update the bottoms for the next iteration
+        bottoms += counts  # Update for stacked bar's position
 
     ax.set_yticks(np.arange(len(y_labels)))
     ax.set_yticklabels(y_labels)
     ax.get_xaxis().set_visible(False)
     plt.ylabel("Stimulus")
-    ax.set_title('Distribution of Responses for Each Stimulus with Intensities: ' + str(intensities))
+    ax.set_title(f'Distribution of Responses for Each Stimulus with Intensities: {intensities}')
     ax.legend(loc='upper center', bbox_to_anchor=(0.5, 0))
-
-    # Adjust layout to fit within figure size
     plt.tight_layout()
-
-    # Show the plot
     plt.savefig(f'response_distribution_{intensities}.png')
 
 
 if __name__ == "__main__":
-
-    # Read the CSV file
+    # Load data
     df = pd.read_csv("../../merge/likert_merged.csv")
 
+    # Specified variations of intensities
     intensities_variation = [[0.49, 0.5, 0.51], [0.49], [0.5], [0.51]]
+
+    # Process each variation
     for intensities in intensities_variation:
         avg_response_per_stimulus(df, intensities)
         avg_response_per_participant(df, intensities)
         response_distribution(df, intensities)
-
