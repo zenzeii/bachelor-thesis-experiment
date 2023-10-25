8fee251ba859aacdfbc9dd640aa9ad5b3b50bc52
Hafidz Arifin <hafidz.harifin@gmail.com>
2023-10-25 12:04:59 +0200

    All functions created (need to be polished)

--- analysis2/analysis.py
+++ analysis2/analysis.py
@@ -5,6 +5,7 @@ import pandas as pd
 import seaborn as sns
 from PIL import Image
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 
 def combine_multiple_csv(skip_participants, invert_likert_response_participants):
@@ -133,16 +134,16 @@ def matching_scatterplot(df):
     Generate a boxplot illustrating the results for each participant and stimulus.
     """
 
-    # Define y-axis limits
-    ymin = df['intensity_match'].min()
-    ymax = df['intensity_match'].max()
-
     # get order
     order = get_stim_order_for_matching(df)
 
     # Filter rows based on given intensities
     df_filtered = df.copy()
     df_filtered = df_filtered[~df_filtered['stim'].str.contains('catch')]
+
+    # Define y-axis limits
+    ymin = df_filtered['intensity_match'].min()
+    ymax = df_filtered['intensity_match'].max()
 
     # Combine 'stim' and 'presented_intensity' for the x-axis
     df_filtered['stim_with_intensity'] = df_filtered['stim'] + '_' + df_filtered['presented_intensity'].astype(str).str.split(".").str[0]
@@ -155,7 +156,7 @@ def matching_scatterplot(df):
     palette_dict_box = {'Right': cmap(0.79), 'Left': cmap(0.21)}
 
     # Create a plot
-    plt.figure(figsize=(15, 12))  # Adjust the figure size as needed
+    plt.figure(figsize=(12.5, 6))
     sns.boxplot(x='stim_with_intensity', y='intensity_match', hue='target_side', data=df_filtered,
                 orient='v', palette=palette_dict_box, hue_order=['Left', 'Right'], order=order_updated)
     sns.despine(left=True)
@@ -199,7 +200,7 @@ def matching_scatterplot(df):
     ax.get_legend().remove()
     ax.set_xlim(-0.5, len(order_updated) - 0.5)   # Set the x-axis limits to remove unused space
     ax.set_xticks(ticks=range(len(xlables)), labels=xlables, rotation=0)
-    ax.set_yticks(ticks=range(0, 251, 10), labels=range(0, 251, 10), rotation=0)
+    ax.set_yticks(ticks=range(0, 201, 10), labels=range(0, 201, 10), rotation=0)
     plt.tight_layout()
 
     x = range(54)
@@ -208,7 +209,7 @@ def matching_scatterplot(df):
         if os:
             plt.axvspan(x0-0.5, x1-0.5, color='gray', alpha=0.2, lw=0)
 
-    plt.savefig(f'{target}matching_scatterplot_boxplot.png')
+    plt.savefig(f'{target}matching_scatterplot_boxplot.svg')
     plt.close()
 
     return
@@ -242,13 +243,154 @@ def matching_connected_scatterplot(df):
     plt.xticks(rotation=45)
     plt.yticks(range(math.floor(int(ymin) / 10) * 10, int(ymax)+10, 10))
     plt.tight_layout()
-    plt.savefig(f'{target}matching_scatterplot_raw.svg')
+    plt.savefig(f'{target}matching_scatterplot_AA_AH_.svg')
 
 
 def matching_mean_adjustments():
+    """
+        Generate a scatter plot showing the average adjustment from participants for each stimulus.
+    """
+
+    # Group by 'presented_intensity' and compute the mean for 'intensity_match'
+    means_all = df.groupby(['presented_intensity', 'target_side', 'stim'])['intensity_match'].mean()
+
+    # Extract y_min and y_max
+    y_min = means_all.min()
+    y_max = means_all.max()
+
+    # Filter rows based on given intensities
+    df_filtered = df.copy()
+    order = get_stim_order_for_matching(df)
+
+    # Calculate means
+    means = df_filtered.groupby(['stim', 'target_side'])['intensity_match'].mean().reset_index()
+    means = means[means['stim'].isin(order)]
+
+    # Map each unique 'stim' to an index
+    stim_to_index = {stim: index for index, stim in enumerate(order)}
+
+    # Adjust x-values for 'Right' and 'Left' to avoid overlap
+    means['x_adjust'] = means.apply(
+        lambda row: stim_to_index[row['stim']] + 0.1 if row['target_side'] == 'Right' else stim_to_index[
+                                                                                               row['stim']] - 0.1,
+        axis=1)
+
+    # Create a plot
+    plt.figure(figsize=(10, 6))
+    ax = sns.scatterplot(x='x_adjust', y='intensity_match', hue='target_side', data=means, palette={'Right': cmap(0.99), 'Left': cmap(0.01)}, s=200, zorder=1)
+    sns.despine(left=True)
+    ax.set_ylim(y_min-5, y_max+2)
+
+
+    # Add stimuli images at the bottom
+    ax2 = ax.twinx()
+    ax2.tick_params(top=False, labeltop=False, left=False, labelleft=False, right=False, labelright=False, bottom=False, labelbottom=False)
+
+
+    # Add horizontal lines for each intensity value
+    for intensity in intensities:
+        ax.axhline(y=intensity, color='grey', linestyle='--', alpha=0.6, lw=1.5)
+
+    # Normalize intensity_match values to [0, 1]
+    means['normalized_intensity'] = means['intensity_match'] / 100.0
+
+    # Draw vertical bars for each 'stim'
+    for stim, x_val in stim_to_index.items():
+        # Existing vertical bar code
+        ax.axvline(x=x_val - 0.1, color='#f0f0f0', ymin=0, ymax=1, lw=10, zorder=0)
+        ax.axvline(x=x_val + 0.1, color='#f0f0f0', ymin=0, ymax=1, lw=10, zorder=0)
+
+        # Extract intensity_match values for the current stim and each target side
+        left_intensity = means[(means['stim'] == stim) & (means['target_side'] == 'Left')]['intensity_match'].values[0]
+        right_intensity = means[(means['stim'] == stim) & (means['target_side'] == 'Right')]['intensity_match'].values[0]
+
+        # Add the intensity_match values on the vertical bars
+        ax.text(x_val - 0.1, y_max-2, f"{left_intensity:.2f}", ha="center", va="center", rotation=90, color="black", fontsize=8)
+        ax.text(x_val + 0.1, y_max-2, f"{right_intensity:.2f}", ha="center", va="center", rotation=90, color="black", fontsize=8)
+
+        # Add stimuli fig
+        image = Image.open(f"../experiment/stim/{stim}.png")
+        imagebox = OffsetImage(image, zoom=0.1)  # Adjust the zoom factor as needed
+        ab = AnnotationBbox(imagebox, (x_val, 0), box_alignment=(0.5, -0.1), frameon=False)
+        ax2.add_artist(ab)
+
+    # Customize the plot appearance
+    ax.set_ylabel('Average adjusted luminance by participants in cd/m²')
+    ax.set_xlabel('Stimulus')
+    ax.set_xticks(range(len(stim_to_index)))
+    ax.set_xticklabels(stim_to_index.keys(), rotation=45, ha='right')
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=2)
+    y_ticks = np.arange(y_min - (y_min % 5), y_max + 5,5)
+    ax.set_yticks(y_ticks)
+
+    # Customize the plot appearance
+    plt.tight_layout()
+    plt.savefig(f'{target}matching_mean_adjustments.svg')
+    plt.close()
     return
 
 def matching_absolute_differences():
+    """
+    Generate a scatter plot showing the average adjust_diff for each stimulus.
+    """
+
+    df_filtered = df.copy()
+    df_filtered = df_filtered[~df_filtered['stim'].str.contains('catch')]
+
+    # Group by 'participant', 'stim', 'presented_intensity' and calculate difference in 'intensity_match'
+    diffs = df_filtered.groupby(['participant', 'stim', 'presented_intensity'])['intensity_match'].diff().dropna()
+
+    # Attach the differences back to the main dataframe
+    df_filtered['intensity_diff'] = diffs
+
+    # Group by 'stim' and calculate the mean difference
+    avg_adjust_diff = df_filtered.groupby('stim')['intensity_diff'].mean().abs()
+
+    sorted_stim = avg_adjust_diff.sort_values(ascending=False).index.tolist()
+
+    # Plotting
+    fig, ax = plt.subplots(figsize=(9, 6))
+
+    for i, stim in enumerate(sorted_stim, start=1):
+        x = avg_adjust_diff[stim]
+        y = i
+        norm_value = (x - (0)) / (10 - (-10))  # Normalizing between -0.1 to 0.1
+        color = LinearSegmentedColormap.from_list("luminance", ["white", "black"])(norm_value)
+
+        ax.scatter(x, y, s=1500, c=[color], alpha=0.5, label=stim)
+        ax.text(x-0.25, y, round(x, 2), va='center')
+
+    ax.set_ylabel("Stimulus")
+    ax.set_xlabel("Average difference of adjustments in cd/m²")
+    ax.set_yticks(range(1, len(sorted_stim) + 1))
+    ax.set_yticklabels(sorted_stim)
+    ticks = np.arange(0, 10 + 1, 2)
+    ax.set_xticks(ticks)
+    #ax.axvline(x=0, color="black", linestyle="--")
+
+    # Create a second y-axis for stimuli images
+    ax2 = ax.twinx()
+    ax2.set_ylim(ax.get_ylim())
+    ax2.set_yticks(ax.get_yticks())
+    ax2.set_yticklabels(["" for _ in sorted_stim])
+
+    # Adding stimuli images next to y-labels on ax2
+    for index, stimulus in enumerate(sorted_stim):
+        if 'catch' in stimulus:
+            continue
+        image = Image.open(f"../experiment/stim/{stimulus}.png")
+        imagebox = OffsetImage(image, zoom=0.13)
+        ab = AnnotationBbox(imagebox, (10, ax2.get_yticks()[index]), frameon=False, boxcoords="data",
+                            pad=0, box_alignment=(-0.05, 0.5))
+        ax2.add_artist(ab)
+
+    ax.set_zorder(ax2.get_zorder() + 1)
+    plt.tight_layout()
+    ax.grid(True, axis='both')
+    plt.subplots_adjust(left=0.25)
+    plt.savefig(f'{target}matching_absolute_differences.svg')
+    plt.close()
+
     return
 
 def liker_heatmap(df, cmap, target):
@@ -388,28 +530,186 @@ def liker_heatmap(df, cmap, target):
 
     return
 
-def liker_median_responses():
+def liker_median_responses(df):
+    """
+    Generate a scatter plot showing the median response for each stimulus.
+    """
+
+    # Filter rows based on given intensities
+    df_filtered = df.copy()
+    df_filtered = df_filtered[~df_filtered['stim'].str.contains('catch')]
+
+    # Group by 'stim' and compute average response
+    avg_response = df_filtered.groupby("stim")["response"].median()
+
+    # Sort stimuli by the computed average response
+    sorted_stim = avg_response.sort_values(ascending=False).index.tolist()
+
+    # Plotting
+    fig, ax = plt.subplots(figsize=(9, 6))
+
+    for i, stim in enumerate(sorted_stim, start=1):
+        x = avg_response[stim]
+        y = i
+        norm_value = (x - (-2)) / (2 - (-2))  # Normalizing between -2 to 2
+        color = cmap(norm_value)
+
+        ax.scatter(x, y, s=1500, c=[color], alpha=0.5, label=stim)
+        ax.text(x-0.1, y, round(x, 2), va='center')
+
+    ax.set_ylabel("Stimulus")
+    ax.set_xlabel("Median response")
+    ax.set_yticks(range(1, len(sorted_stim) + 1))
+    ax.set_yticklabels(sorted_stim)
+    ax.set_xticks(range(-2, 3))
+    ax.axvline(x=0, color="black", linestyle="--")
+
+    # Create a second y-axis for stimuli images
+    ax2 = ax.twinx()
+    ax2.set_ylim(ax.get_ylim())
+    ax2.set_yticks(ax.get_yticks())
+    ax2.set_yticklabels(["" for _ in sorted_stim])
+
+    # Adding stimuli images next to y-labels on ax2
+    for index, stimulus in enumerate(sorted_stim):
+        image = Image.open(f"../experiment/stim/{stimulus}.png")
+        imagebox = OffsetImage(image, zoom=0.13)
+        ab = AnnotationBbox(imagebox, (2, ax2.get_yticks()[index]), frameon=False, boxcoords="data",
+                            pad=0, box_alignment=(-0.05, 0.5))
+        ax2.add_artist(ab)
+
+    ax.set_zorder(ax2.get_zorder() + 1)
+    plt.tight_layout()
+    ax.grid(True, axis='both')
+    plt.savefig(f'{target}likert_median_response.svg')
+    plt.close()
+
+    return sorted_stim
+
     return
 
 def liker_distribution():
+    """
+    Display the distribution of responses for each stimulus.
+    """
+
+    # Filter data based on intensities
+    df_filtered = df.copy()
+    df_filtered = df_filtered[~df_filtered['stim'].str.contains('catch')]
+
+    # Colors for the likert scale responses
+    color_map = {
+        -2: cmap(0.1),
+        -1: cmap(0.3),
+        0: cmap(0.5),
+        1: cmap(0.7),
+        2: cmap(0.9)
+    }
+
+    # Legend for the responses
+    legend = ['Left target is definitely brighter', 'Left target is maybe brighter', 'Targets are equally bright',
+              'Right target is maybe brighter', 'Right target is definitely brighter']
+
+    # Aggregate data
+    grouped_data = df_filtered.groupby(['stim', 'response']).size().unstack(fill_value=0)
+
+
+    sorted_data = grouped_data.sort_values(by=list(color_map.keys()), ascending=True)
+
+    # Bar chart for visualization
+    fig, ax = plt.subplots(figsize=(10, 8))
+    y_labels = sorted_data.index.tolist()
+    bar_width = 0.6
+    bottoms = np.zeros(len(y_labels))
+
+    for i, likert_value in enumerate(sorted_data.columns):
+        color = color_map.get(likert_value, 'white')
+        counts = sorted_data[likert_value]
+        bars = ax.barh(np.arange(len(y_labels)), counts, color=color, label=legend[int(likert_value + 2)],
+                       height=bar_width, left=bottoms)
+
+        # Labeling bars
+        for bar, value in zip(bars, counts):
+            if value != 0:
+                ax.text(bar.get_x() + bar.get_width() / 2, bar.get_y() + bar.get_height() / 2, str(value), ha='center',
+                        va='center', color='black', fontsize=10)
+
+        bottoms += counts  # Update for stacked bar's position
+
+    ax.set_yticks(np.arange(len(y_labels)))
+    ax.set_yticklabels(y_labels)
+
+    # Now set the y-ticks and labels
+    ax2 = ax.twinx()
+    ax.set_yticks(np.arange(len(y_labels)))
+    ax.set_yticklabels(y_labels)
+    y_position_stim = range(0, (len(y_labels)+1))
+    ax2.set_yticks(y_position_stim)
+    ax2.set_yticklabels(["                       " for _ in y_position_stim])
+
+    # Calculate the maximum x-value from the data
+    max_x_value = sorted_data.sum(axis=1).max()
+    print(max_x_value)
+
+    # Adding stimuli images next to y-labels
+    for index, stimulus in enumerate(y_labels):
+        image = Image.open(f"../experiment/stim/{stimulus}.png")
+        imagebox = OffsetImage(image, zoom=0.12)
+        ab = AnnotationBbox(imagebox, (max_x_value, y_position_stim[index]), frameon=False, boxcoords="data",
+                            box_alignment=(-0.05, -0.3), pad=0)
+        ax2.add_artist(ab)
+
+    # Set the x-axis limits to end at
+    ax.set_xlim(right=max_x_value)
+    ax.get_xaxis().set_visible(False)
+
+    # Set y-axis limits
+    ax.set_ylim(-0.3, len(y_labels) - 0.7)
+
+    plt.ylabel("Stimulus")
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, 0))
+    plt.tight_layout()
+    plt.savefig(f'{target}likert_distribution.svg')
+    plt.close()
     return
 
+def iqr2(df):
+    # Function to compute IQR
+    def compute_iqr(data, column_name):
+        return data.groupby('stim')[column_name].agg(lambda x: round((x.quantile(0.75) - x.quantile(0.25)), 2))
 
+    # Filter out rows that contain 'catch' in 'stim'
+    df_copy = df.copy()
+    df_copy = df_copy[~df_copy['stim'].str.contains('catch')]
 
+    # Compute IQR for 'matching'
+    matching_df = df_copy[df_copy['trial'].str.contains('matching')]
+    matching_iqr = compute_iqr(matching_df, 'intensity_match')
 
+    # Compute IQR for 'direction'
+    direction_df = df_copy[df_copy['trial'].str.contains('direction')]
+    direction_iqr = compute_iqr(direction_df, 'response')
 
+    # Merge the two IQR values on 'stim'
+    result = pd.DataFrame(matching_iqr).rename(columns={'intensity_match': 'iqr_matching'})
+    result['iqr_direction'] = direction_iqr
+    result = result.reset_index()
+    result = result.sort_values(by='iqr_matching')
+
+    print(result)
 
 
 if __name__ == "__main__":
 
     # discard participants from analysis
-    skip_participants = []#["AA"]
+    skip_participants = ["AA", "KP"]
 
     # invert likert responses for these participants
-    invert_likert_response_participants = []#["SP"]
+    invert_likert_response_participants = ["SP"]
 
     # Gather all data for the analysis
     df = combine_multiple_csv(skip_participants, invert_likert_response_participants)
+    intensities = sorted(df['presented_intensity'].unique())
 
     # Create common colormap
     cmap = sns.diverging_palette(250, 10, as_cmap=True)
@@ -419,15 +719,16 @@ if __name__ == "__main__":
 
     """
     START PLOTTING HERE
+    Remove '#' when needed
     """
 
-    matching_connected_scatterplot(df)
-    #matching_scatterplot(df)
+    #matching_mean_adjustments() # Done
+    #matching_absolute_differences() # Done
+    #matching_connected_scatterplot(df) # Started waiting for Email/Feedback
+    #matching_scatterplot(df) # Done ( AH discard or not?)
 
-    #matching_mean_adjustments()
-    #matching_absolute_differences()
+    #liker_median_responses(df) # Done
+    #liker_distribution() # Done
+    #liker_heatmap(df, cmap, target) # Done
 
-    #liker_heatmap(df, cmap, target)
-    #liker_median_responses()
-    #liker_distribution()
-
+    #iqr2(df)
